4f2509bb7661d969094eed6d52e9243d39cc2e0c
raindragonn <raindragonn@gmail.com>
2023-08-28 15:52:35 +0900

    feat: 스프링 부트 서블릿 환경 구성

--- src/main/java/raindragonn/servlet/ServletApplication.java
+++ src/main/java/raindragonn/servlet/ServletApplication.java
@@ -2,7 +2,10 @@ package raindragonn.servlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+// 서블릿을 스캔하고 등록해준다.(자동 등록)
+@ServletComponentScan
 @SpringBootApplication
 public class ServletApplication {
 
