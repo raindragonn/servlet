63a0712e452a9166f348ef7585f6f766dc7d44c0
raindragonn <raindragonn@gmail.com>
2023-08-28 16:58:26 +0900

    feat: messageBody json 데이터 조회

---
+++ src/main/java/raindragonn/servlet/basic/Request/RequestBodyJsonServlet.java
@@ -0,0 +1,31 @@
+package raindragonn.servlet.basic.Request;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletInputStream;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.util.StreamUtils;
+import raindragonn.servlet.basic.HelloData;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@WebServlet(name = "RequestBodyJsonServlet", urlPatterns = "/request-body-json")
+public class RequestBodyJsonServlet extends HttpServlet {
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ServletInputStream inputStream = request.getInputStream();
+        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+        System.out.println("messageBody = " + messageBody);
+
+        HelloData helloData = objectMapper.readValue(messageBody, HelloData.class);
+
+        System.out.println("helloData = " + helloData.toString());
+        response.getWriter().write("OK");
+    }
+}
